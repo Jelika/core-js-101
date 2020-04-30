482eb14255313e6fdff56befc55d41a131601e85
Jelika <smirnova.lika.23092001@gmail.com>
2020-04-30 22:56:10 +0300

    Saving files before refreshing line endings

--- test/01-strings-tests.js
+++ test/01-strings-tests.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const tasks = require('../src/01-strings-tasks');
 it.optional = require('../extensions/it-optional');
 
-describe('01-strings-tasks', () => {
+describe.only('01-strings-tasks', () => {
   it.optional('concatenateStrings should return concatenation of two strings', () => {
     assert.equal('aabb', tasks.concatenateStrings('aa', 'bb'));
     assert.equal('aa', tasks.concatenateStrings('aa', ''));
